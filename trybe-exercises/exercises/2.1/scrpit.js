c4009efd32797428204cc3bf026a5cca6d23d47d
Felipe Gomes Belarmino <70665848+felipebelarmino@users.noreply.github.com>
2020-10-07 20:08:18 -0300

    Solicitar alteração da linha 5 e 3

--- trybe-exercises/exercises/2.1/scrpit.js
+++ trybe-exercises/exercises/2.1/scrpit.js
@@ -2,6 +2,6 @@ let student = "euMesmo";
 let studentSkills = 10;
  javascript-updates
 "Vou ter muito sucesso na programação!"
-master
+//E ai Ruan blz, estou mandando um pull request para solicitar alteração da linha 5 e 3 pois podem dar conflito ao executar o script. Valeu. Felipe G. Belarmino - Turma 8.
 console.log("Sucesso!");
 
