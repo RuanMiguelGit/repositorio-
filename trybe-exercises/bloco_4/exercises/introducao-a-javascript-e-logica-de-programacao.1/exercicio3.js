90769184358ab29e4d6434da41eca1fb07c10c4c
Ruan Miguel <ruanmiguelps@gmail.com>
2020-10-26 14:38:23 -0300

    exercicio 3 resolvido

---
+++ trybe-exercises/bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao.1/exercicio3.js
@@ -0,0 +1,11 @@
+let a = 30;
+let b = 50;
+let c = 60;
+
+if (a >=  b && a >= c) {
+    console.log ( "maior numero é o a")
+} else if ( b >= a && b >= c) {
+    console.log ( "maior numero é o b")
+} else {
+    console.log (" o maior numero é o c")
+}
