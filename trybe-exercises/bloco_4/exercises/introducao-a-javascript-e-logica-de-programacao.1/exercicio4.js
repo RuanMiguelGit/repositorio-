9de9d5a2056f8238c5650afa688a5d2848acf042
Ruan Miguel <ruanmiguelps@gmail.com>
2020-10-26 19:11:25 -0300

    exercicio 5 resolvido

---
+++ trybe-exercises/bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao.1/exercicio4.js
@@ -0,0 +1,11 @@
+let numerodefinido = 85;
+
+if ( numerodefinido >=0) {
+    console.log( " o numero definido é positivo");
+} else if (numerodefinido < 0) {
+    console.log ( "o numero definido é negativo");
+} else {
+    console.log ( "o numero definido é zero ") ;
+}
+
+
---
+++ trybe-exercises/bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao.1/exercicio5.js
@@ -0,0 +1,13 @@
+let angulo1 = 60;
+let angulo2 = 60;
+let angulo3 = 60;
+
+let resultado;
+
+if ( angulo1 + angulo2 +angulo3 === 180) {
+   resultado = true;
+} else  {
+    resultado = false; 
+}
+
+console.log (resultado);
