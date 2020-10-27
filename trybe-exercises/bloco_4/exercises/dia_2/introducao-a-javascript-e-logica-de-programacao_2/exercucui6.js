865adce217a8ab5656d3c40b66f111e8c4057cd5
Ruan Miguel <ruanmiguelps@gmail.com>
2020-10-27 13:32:01 -0300

    exercicio 6 resolvido

---
+++ trybe-exercises/bloco_4/exercises/dia_2/introducao-a-javascript-e-logica-de-programacao_2/exercucui6.js
@@ -0,0 +1,15 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let impar = 0;
+
+for ( index = 0; index < numbers.length; index += 1 ) {
+    if ( numbers[index] % 2 === 1) {
+        console.log (numbers[index] )
+    } else if ( numbers[index] % 2 === 0) {
+        console.log ( "nenhum valor impar encontrado")
+    
+    }
+
+    
+}
+
+
