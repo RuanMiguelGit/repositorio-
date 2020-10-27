2cbbefae1d63c4c67833634d7baf213f4be5e8c6
Ruan Miguel <ruanmiguelps@gmail.com>
2020-10-26 21:19:57 -0300

    exercicio 8 realizado

---
+++ trybe-exercises/bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao.1/exercicio8.js
@@ -0,0 +1,13 @@
+let a = 6
+let b = 5
+let c = 7
+
+let resultado;
+
+if ( a % 2 === 0  || b % 2 === 0 || c % 2 === 0 ) {
+    resultado = true ;
+} else {
+    resultado = false;
+}
+
+console.log(resultado);
