0b44471b13ddc6d3f497690d5e3b1c6dfd62fc4a
Ruan Miguel <ruanmiguelps@gmail.com>
2020-10-28 08:51:23 -0300

    exercicio 2 resolvido

--- trybe-exercises/bloco_4/exercises/dia_3/introducao-a-javascript-e-logica-de-programacao_3/exercicio1.js
+++ trybe-exercises/bloco_4/exercises/dia_3/introducao-a-javascript-e-logica-de-programacao_3/exercicio1.js
@@ -1,13 +1,8 @@
-let coluna = 5;
-let linha = 5;
 
-     
-
-
-for (let col = 1; col <= 5; col += 1) {
-    let str = "";
-      for  (let lin = 1 ;  lin <= 5; lin += 1 ) {
-    str += "*";
+for (let col = 1; col <= 5; col += 1){ 
+  let str = ""                  
+      for  (let lin = 1 ;  lin <= 5; lin += 1 )  {
+           str += "*";
        }
    console.log(str);
   }
