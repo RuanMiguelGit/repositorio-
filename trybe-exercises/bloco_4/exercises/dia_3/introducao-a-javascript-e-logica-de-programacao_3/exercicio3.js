4e275bd6db7c7241f1c4e99a1b35bc3f756cfa4c
Ruan Miguel <ruanmiguelps@gmail.com>
2020-10-28 13:07:41 -0300

    exercicio 3 realizado

--- trybe-exercises/bloco_4/exercises/dia_3/introducao-a-javascript-e-logica-de-programacao_3/exercicio3.js
+++ trybe-exercises/bloco_4/exercises/dia_3/introducao-a-javascript-e-logica-de-programacao_3/exercicio3.js
@@ -1,10 +1,15 @@
-
-for (let col = 5; col >= 1; col -= 1){ 
-    let str = ""                  
+let n = 5; 
+for (let col = 1; col <= 5; col += 1){ 
+    let str = ""    
+    let spaces =" "  
+    for ( space =1; space <= (n - col) ; space += 1 )  {
+       
+     spaces += " ";
+    }          
         for  (let lin = 1 ;  lin <= col; lin += 1 )  {
              str += "*";
          }
-     console.log(str);
+     console.log(spaces + str);
     }
   
   
---
+++ trybe-exercises/bloco_4/exercises/dia_3/introducao-a-javascript-e-logica-de-programacao_3/exerciciobonus.js
@@ -0,0 +1,10 @@
+
+for (let col = 5; col >= 1; col -= 1){ 
+    let str = ""                  
+        for  (let lin = 1 ;  lin <= col; lin += 1 )  {
+             str += "*";
+         }
+     console.log(str);
+    }
+  
+  
