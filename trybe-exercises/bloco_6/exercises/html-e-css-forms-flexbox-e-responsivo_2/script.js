89dae77e47b8422524e72542e977897dca007065
Ruan Miguel <ruanmiguelps@gmail.com>
2020-11-13 20:07:26 -0300

    começo

---
+++ trybe-exercises/bloco_6/exercises/html-e-css-forms-flexbox-e-responsivo_2/script.js
@@ -0,0 +1,49 @@
+let options = document.getElementById('T-shirt-size') 
+function AllStates () {
+let estados = {
+
+        'AC': 'Acre',
+        'AL': 'Alagoas',
+        'AP': 'Amapá',
+        'AM': 'Amazonas',
+        'BA': 'Bahia',
+        'CE': 'Ceará',
+        'DF': 'Distrito Federal',
+        'ES': 'Espírito Santo',
+        'GO': 'Goiás',
+        'MA': 'Maranhão',
+        'MT': 'Mato Grosso',
+        'MS': 'Mato Grosso do Sul',
+        'MG': 'Minas Gerais',
+        'PA': 'Pará',
+        'PB': 'Paraíba',
+        'PR': 'Paraná',
+        'PE': 'Pernambuco',
+        'PI': 'Piauí',
+        'RJ': 'Rio de Janeiro',
+        'RN': 'Rio Grande do Norte',
+        'RS': 'Rio Grande do Sul',
+        'RO': 'Rondônia',
+        'RR': 'Roraima',
+        'SC': 'Santa Catarina',
+        'SP': 'São Paulo',
+        'SE': 'Sergipe',
+        'TO': 'Tocantins'
+}
+// }
+// for(let key in estados){
+//     console.log(estados,key)
+// }
+
+
+for (let key in estados) {
+let states = estados[key]
+let elemento = document.createElement('option')
+elemento.innerHTML = estados[key]
+elemento.value=(key)
+options.appendChild(elemento)
+}
+
+}
+
+AllStates()
