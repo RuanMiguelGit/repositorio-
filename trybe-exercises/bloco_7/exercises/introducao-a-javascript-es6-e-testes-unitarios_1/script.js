bf3f6c0139bb9ad9207f6d234dd5b2642baf1686
Ruan Miguel <ruanmiguelps@gmail.com>
2020-11-21 20:04:37 -0300

    ] ex2 parte 2

--- trybe-exercises/bloco_7/exercises/introducao-a-javascript-es6-e-testes-unitarios_1/script.js
+++ trybe-exercises/bloco_7/exercises/introducao-a-javascript-es6-e-testes-unitarios_1/script.js
@@ -1,26 +1,26 @@
 
 // //Parte 1
-// const testingScope = escopo => {
-//     if (escopo === true) { 
-//     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-//     ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
-//     console.log(ifScope);
-//   } else {
-//     const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-//     console.log(elseScope);
-//   }
-//   // console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
-// }
+    const testingScope = escopo => {
+    if (escopo === true) { 
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+    console.log(ifScope);
+  } else {
+    const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+  // console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+}
 
-// testingScope(true);
+testingScope(true);
 
 
-// const oddsAndEvens = [13, 3, 4, 10, 7, 2];
-// const Compare = (a, b) =>  a-b 
-// oddsAndEvens.sort(Compare)
-// console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente !`);
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+const Compare = (a, b) =>  a-b 
+oddsAndEvens.sort(Compare)
+console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente !`);
 
-// //Parte 2 
+//Parte 2 
 
 const Factorial = num => {
     let result = 1
@@ -53,3 +53,21 @@ const factorial2 = num => {if(num<=1) return 1
     return num*factorial2(num-1) }
 
    console.log(factorial2(5))
+
+// longestWord
+
+const longestWord = phrase => {
+    
+let Longest = " ";
+const words = phrase.split(' ')
+for( let index = 0; index < words.length; index +=1){
+    if( words[index].length > Longest.length) {
+        Longest = words[index]
+    }
+   
+}
+
+return Longest
+}
+
+console.log(longestWord("Antônio foi no banheiro e não sabemos o que aconteceu"))
