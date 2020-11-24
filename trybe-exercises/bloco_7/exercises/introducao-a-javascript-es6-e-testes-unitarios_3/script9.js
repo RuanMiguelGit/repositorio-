5e58b034b93dd864f46c5bec3bd323f788df4065
Ruan Miguel <ruanmiguelps@gmail.com>
2020-11-24 19:32:48 -0300

    NAO E DE MINHA ALTORIA

---
+++ trybe-exercises/bloco_7/exercises/introducao-a-javascript-es6-e-testes-unitarios_3/script9.js
@@ -0,0 +1,30 @@
+const assert = require('assert');
+// escreva a função findTheNeedle aqui
+
+
+const findTheNeedle = (words, item) => {
+    let output = -1
+    for ( let index =0; index < words.length; index +=1) {
+        if ( item === words[index]) {
+            output = index
+            console.log(output)
+}
+    }
+    return output
+
+}
+
+let words = ['house', 'train', 'slide', 'needle', 'book'];
+let expected = 3;
+let output = findTheNeedle(words, 'needle');
+assert.strictEqual(output, expected);
+
+words = ['plant', 'shelf', 'arrow', 'bird'];
+expected = 0;
+output = findTheNeedle(words, 'plant');
+assert.strictEqual(output, expected);
+
+words = ['plant', 'shelf', 'arrow', 'bird'];
+expected = -1;
+output = findTheNeedle(words, 'plat');
+assert.strictEqual(output, expected);
