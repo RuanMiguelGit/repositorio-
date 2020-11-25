cc9040d491181a61a131bcbc4ad9b2962092d7cc
Ruan Miguel <ruanmiguelps@gmail.com>
2020-11-25 10:16:42 -0300

    resolvido

---
+++ trybe-exercises/bloco_7/exercises/introducao-a-javascript-es6-e-testes-unitarios_3/script12.js
@@ -0,0 +1,20 @@
+
+const greaterThanTen = (array) => {
+    let results = [];
+    for (let i = 0; i < array.length; i += 1) {
+      if (array[i] > 10) {
+        results.push(array[i])
+      }
+    }
+    return results;
+  };
+  
+  const parameter = [4, 10, 32, 9, 21];
+  const result = [32, 21];
+
+  const output = greaterThanTen(parameter)
+
+
+  const assert = require ('assert')
+
+  assert.deepStrictEqual(output, result)
