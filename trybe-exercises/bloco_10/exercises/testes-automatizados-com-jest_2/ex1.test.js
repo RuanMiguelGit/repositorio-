40243e66be48d263d655e3d628ec8f5e0a182bcb
Ruan Miguel <ruanmiguelps@gmail.com>
2021-01-05 14:56:15 -0300

    ex 1 e 2

---
+++ trybe-exercises/bloco_10/exercises/testes-automatizados-com-jest_2/ex1.test.js
@@ -0,0 +1,10 @@
+const uppercase = require('./ex1')
+
+describe('uppercase', () => {
+    it('it should check if the letters are change to Uppercase', done =>{
+        uppercase('bread', str =>{
+            expect(str).toBe("BREAD")
+            done()
+        } )
+    })
+})
---
+++ trybe-exercises/bloco_10/exercises/testes-automatizados-com-jest_2/ex2.test.js
@@ -0,0 +1,47 @@
+const promiseme = require('./ex2')
+
+const users = {
+    4 : { name: 'Mark' },
+    5 : { name: 'Paul' }
+    };
+    
+    const findUserById = (id) => {
+      return new Promise((resolve, reject) => {
+          if (users[id]) {
+            return resolve(users[id]);
+          }
+    
+          return reject({ error: 'User with ' + id + ' not found.' });
+      });
+    }
+    
+    const getUserName = (userID) => {
+      return findUserById(userID).then(user => user.name);
+    }
+
+
+
+describe('twofuncions', () =>{ 
+    it('if the user is found', ()=>{
+        expect.assertions(1)
+        return getUserName(4).then(listnames => {
+            expect(listnames).toEqual('Mark')
+            // expect(listnames[1].nmae).toEqual('Paul')
+        })
+    })
+        it('if the other user is found', ()=>{
+            expect.assertions(1)
+            return getUserName(5).then(listnames =>{
+                expect(listnames).toEqual("Paul")
+            })
+        })
+})
+
+describe('error tester', ()=>{
+    it('it should check if the user doesnt exist', () =>{
+        expect.assertions(1)
+        return getUserName(7).catch(error => {
+            expect(error).toEqual({ error: 'User with 7 not found.' })
+        })
+    })
+})
